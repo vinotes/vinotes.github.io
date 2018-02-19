43d046b97c6fcde4deb7ae30684dac2d50e726a6
vinodhmjhuman <vinodhmjhuman@gmail.com>
2018-02-20 00:01:03 +0530

    Nikola auto commit.
    Nikola version: 7.8.11

--- conf.py
+++ conf.py
@@ -983,7 +983,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "mindtoilet"
+COMMENT_SYSTEM_ID = "https-mindtoilet-github-io"
 
 # Enable annotations using annotateit.org?
 # If set to False, you can still enable them for individual posts and pages
